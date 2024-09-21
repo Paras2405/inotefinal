02d4f59d3101f02843143ae4eb1e55ceade4f3ad
Paras2405 <159181551+Paras2405@users.noreply.github.com>
2024-09-21 10:05:56 +0530

    App and Navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,28 @@ import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-do
 import LoadingBar from 'react-top-loading-bar'
 
 class App extends Component {
+
+ 
+
   apiKey = import.meta.env.VITE_NEWS_API;
 
   state={
-    progress:0
+    progress:0,
+    mode:"dark"
   }
+
+  togglestate=()=>{
+    if(this.state.mode==="light"){
+      this.setState({mode:'dark'})
+       document.body.style.backgroundColor="black"
+    }
+    else{
+      this.setState({mode:'light'})
+      document.body.style.backgroundColor="white"
+    }
+  }
+
+  
   setProgress=(progress)=>{
      this.setState({progress:progress})
   }
@@ -17,7 +34,7 @@ class App extends Component {
     return (
       <>
         <Router>
-          <Navbar />
+          <Navbar mode={this.state.mode} togglestate={this.togglestate} />
           <LoadingBar
           height={3}
         color='#f11946'
@@ -25,14 +42,14 @@ class App extends Component {
        
       />
           <Routes>
-          <Route path="/" element={<Navigate to="/general" />} />
-            <Route path="/general" element={<News  setProgress={this.setProgress} apiKey={this.apiKey} key="general"pagesize={6} country="us" category="general" />} />
-            <Route path="/business" element={<News  setProgress={this.setProgress} apiKey={this.apiKey}key="bsuiness"pagesize={6} country="us" category="business" />} />
-            <Route path="/entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment"pagesize={6} country="us" category="entertainment" />} />
-            <Route path="/health" element={<News setProgress={this.setProgress} apiKey={this.apiKey}key="health" pagesize={6} country="us" category="health" />} />
-            <Route path="/sports" element={<News  setProgress={this.setProgress}apiKey={this.apiKey} key="sports"pagesize={6} country="us" category="sports" />} />
-            <Route path="/technology" element={<News  setProgress={this.setProgress}apiKey={this.apiKey}key="technology" pagesize={6} country="us" category="technology" />} />
-            <Route path="/science" element={<News setProgress={this.setProgress}apiKey={this.apiKey} key="science" pagesize={6} country="us" category="science" />} />
+          
+            <Route path="/general" element={<News  setProgress={this.setProgress} apiKey={this.apiKey} key="general" pagesize={6} country="us" category="general" />} />
+            <Route path="/business" element={<News  setProgress={this.setProgress} apiKey={this.apiKey} key="business" pagesize={6} country="us" category="business" />} />
+            <Route path="/entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pagesize={6} country="us" category="entertainment" />} />
+            <Route path="/health" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pagesize={6} country="us" category="health" />} />
+            <Route path="/sports" element={<News  setProgress={this.setProgress}apiKey={this.apiKey} key="sports" pagesize={6} country="us" category="sports" />} />
+            <Route path="/technology" element={<News  setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pagesize={6} country="us" category="technology" />} />
+            <Route path="/science" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pagesize={6} country="us" category="science" />} />
           </Routes>
         </Router>
       </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,42 +2,49 @@ import React,{Component} from 'react'
 import {Link} from 'react-router-dom'
 
 class Navbar extends Component{
+  handleToggle = () => {
+    this.props.togglestate();
+  }
     render(){
   return (
-<nav className="navbar navbar-expand-lg bg-dark">
+<nav className={`navbar navbar-expand-lg  bg-${this.props.mode}`}>
   <div className="container-fluid">
-   <Link className="navbar-brand text-light" to="/">TBP News</Link>  
+   <Link className={`navbar-brand text-${this.props.mode==='dark'?'light':'dark'}`} to="/">TBP News</Link>  
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" style={{ filter: 'invert(1)' }}></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-        <Link className="navbar-brand text-light" to="/general">general</Link>  
+        <Link className={`navbar-brand text-${this.props.mode==='dark'?'light':'dark'}`} to="/general">general</Link>  
         </li>
         <li className="nav-item">
-        <Link className="navbar-brand text-light" to="/sports">sports</Link>  
+        <Link className={`navbar-brand text-${this.props.mode==='dark'?'light':'dark'}`} to="/sports">sports</Link>  
         </li>
         <li className="nav-item">
-        <Link className="navbar-brand text-light" to="/technology">technology</Link>  
+        <Link className={`navbar-brand text-${this.props.mode==='dark'?'light':'dark'}`} to="/technology">technology</Link>  
         </li>
         <li className="nav-item">
-        <Link className="navbar-brand text-light" to="/business">business</Link>  
+        <Link className={`navbar-brand text-${this.props.mode==='dark'?'light':'dark'}`} to="/business">business</Link>  
         </li>
         <li className="nav-item">
-        <Link className="navbar-brand text-light" to="/science">science</Link>  
+        <Link className={`navbar-brand text-${this.props.mode==='dark'?'light':'dark'}`} to="/science">science</Link>  
         </li>
         <li className="nav-item">
-        <Link className="navbar-brand text-light" to="/health">health</Link>  
+        <Link className={`navbar-brand text-${this.props.mode==='dark'?'light':'dark'}`}to="/health">health</Link>  
         </li>
         <li className="nav-item">
-        <Link className="navbar-brand text-light" to="/entertainment">entertainment</Link>  
+        <Link className={`navbar-brand text-${this.props.mode==='dark'?'light':'dark'}`} to="/entertainment">entertainment</Link>  
         </li>
         
        
        
       </ul>
-     
+      <div className={`form-check form-switch text-${this.props.mode==='light'?'dark':'light'}`}>
+  <input className="form-check-input"  type="checkbox"  onChange={this.handleToggle} 
+                checked={this.props.mode === 'dark'} role="switch" id="flexSwitchCheckDefault" style={{cursor:"pointer"}}/>
+  <label className="form-check-label" for="flexSwitchCheckDefault" style={{color:this.props.mode==='dark'?'dark':'light'}}>{this.props.mode}</label>
+</div>
     </div>
   </div>
 </nav>
